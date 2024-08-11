b7d9347d7b0101d0bb051711eb424ae820a691d0
minqi6 <149478429+minqi6@users.noreply.github.com>
2024-08-11 15:02:01 +0100

    Add files via upload

---
+++ reciepe_n_health_assisstant/flow_ingredient_breakdown.py
@@ -0,0 +1,19 @@
+from aijson import Flow
+import asyncio
+import sys
+from new_actions import split_text
+
+async def main(food: str):
+    # load the flow
+    flow = Flow.from_file('reciepe_n_health_assisstant/ingredient_breakdown.ai.yaml')
+
+    # set any variables
+    flow = flow.set_vars(food=food)
+
+    # run it
+    result = await flow.run()
+    print(result)
+
+if __name__ == '__main__':
+    food = sys.argv[1]
+    asyncio.run(main(food))
